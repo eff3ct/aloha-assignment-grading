ea93281a6c8c77d98c42c1fe442adabf4f441bf0
ymjoo12 <ymjoo12@naver.com>
2022-08-07 13:43:12 +0900

    feat: crawler, sqlite, data controller

---
+++ db.py
@@ -0,0 +1,151 @@
+import sqlite3
+
+
+class DB:
+    
+    db = None
+    
+    @classmethod
+    def connect(cls):
+        cls.db = sqlite3.connect('aloha.db')
+        return cls.db
+
+    @classmethod
+    def init(cls):
+        cls.connect()
+        cls.db.execute('''
+            CREATE TABLE IF NOT EXISTS members (
+                user_id TEXT PRIMARY KEY,
+                level INTEGER
+            )
+        ''')
+        cls.db.execute('''
+            CREATE TABLE IF NOT EXISTS problems (
+                problem_id INTEGER PRIMARY KEY,
+                title TEXT
+            )
+        ''')
+        cls.db.execute('''
+            CREATE TABLE IF NOT EXISTS submissions (
+                no INTEGER PRIMARY KEY,
+                user_id TEXT,
+                problem_id INTEGER,
+                result TEXT,
+                memory INTEGER,
+                time INTEGER,
+                language TEXT,
+                byte INTEGER,
+                submitted_at INTEGER,
+                CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES members (user_id),
+                CONSTRAINT problem_id_fk FOREIGN KEY (problem_id) REFERENCES problems (problem_id)
+            )
+        ''')
+        cls.db.execute('''
+            CREATE TABLE IF NOT EXISTS practices (
+                practice_id INTEGER PRIMARY KEY,
+                title TEXT,
+                started_at INTEGER,
+                ended_at INTEGER
+            )
+        ''')
+        cls.db.execute('''
+            CREATE TABLE IF NOT EXISTS practice_problems (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                practice_id INTEGER,
+                problem_id INTEGER,
+                no INTEGER,
+                title TEXT,
+                is_required INTEGER,
+                CONSTRAINT practice_id_fk FOREIGN KEY (practice_id) REFERENCES practices (practice_id),
+                CONSTRAINT problem_id_fk FOREIGN KEY (problem_id) REFERENCES problems (problem_id)
+            )
+        ''')
+        cls.db.commit()
+    
+    @classmethod
+    def tables(cls):
+        cls.connect()
+        return [
+            table for table, 
+            in cls.db.execute('SELECT name FROM sqlite_master WHERE type="table"').fetchall()
+            if table != 'sqlite_sequence'
+        ]
+
+    @classmethod
+    def drop(cls, table):
+        cls.connect()
+        cls.db.execute(f'DROP TABLE {table}')
+        cls.db.commit()
+        
+    @classmethod
+    def insert_member(cls, user_id):
+        cls.init()
+        cls.db.execute('''
+            INSERT INTO members (user_id) VALUES (?)
+        ''', (user_id,))
+        cls.db.commit()
+        
+    @classmethod
+    def insert_problem(cls, problem_id, title=None):
+        cls.init()
+        if cls.db.execute(f'SELECT * FROM problems WHERE problem_id={problem_id}').fetchone():
+            return
+        cls.db.execute('''
+            INSERT INTO problems (problem_id, title) VALUES (?, ?)
+        ''', (problem_id, title))
+        cls.db.commit()
+    
+    @classmethod
+    def insert_submission(cls, no, user_id, problem_id, result, memory, time, language, byte, submitted_at):
+        cls.init()
+        cls.db.execute('''
+            INSERT INTO submissions (no, user_id, problem_id, result, memory, time, language, byte, submitted_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (no, user_id, problem_id, result, memory, time, language, byte, submitted_at))
+        cls.db.commit()
+        
+    @classmethod
+    def insert_practice(cls, practice_id, title, started_at, ended_at):
+        cls.init()
+        cls.db.execute('''
+            INSERT INTO practices (practice_id, title, started_at, ended_at) VALUES (? ,?, ?, ?)
+        ''', (practice_id, title, started_at, ended_at))
+        cls.db.commit()
+    
+    @classmethod
+    def insert_practice_problem(cls, practice_id, problem_id, no, title):
+        cls.init()
+        cls.db.execute('''
+            INSERT INTO practice_problems (practice_id, problem_id, no, title) VALUES (?, ?, ?, ?)
+        ''', (practice_id, problem_id, no, title))
+        cls.db.commit()
+        
+    @classmethod
+    def select_members(cls):
+        cls.init()
+        return cls.db.execute('SELECT * FROM members').fetchall()
+    
+    @classmethod
+    def select_problems(cls):
+        cls.init()
+        return cls.db.execute('SELECT * FROM problems').fetchall()
+    
+    @classmethod
+    def select_submissions(cls, user_id, started_at, ended_at):
+        cls.init()
+        return cls.db.execute('SELECT * FROM submissions WHERE user_id = ? AND submitted_at BETWEEN ? AND ? GROUP BY problem_id', (user_id, started_at, ended_at)).fetchall()
+    
+    @classmethod
+    def get_practice(cls, practice_id):
+        cls.init()
+        return cls.db.execute('SELECT * FROM practices WHERE practice_id = ?', (practice_id,)).fetchone()
+    
+    @classmethod
+    def select_practices(cls):
+        cls.init()
+        return cls.db.execute('SELECT * FROM practices').fetchall()
+    
+    @classmethod
+    def select_practice_problems(cls, practice_id):
+        cls.init()
+        return cls.db.execute('SELECT * FROM practice_problems WHERE practice_id = ?', (practice_id,)).fetchall()
+
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import json
-import sqlite3
-import requests
-import streamlit as st
-from bs4 import BeautifulSoup as bs
+import boj
+from db import DB
+
 from pygments import highlight
 from pygments.lexers import JsonLexer
 from pygments.formatters import TerminalFormatter
@@ -14,70 +13,68 @@ class Utils:
     def print_json(json_data):
         json_str = json.dumps(json_data, indent=4, sort_keys=True, ensure_ascii=False)
         print(highlight(json_str, JsonLexer(), TerminalFormatter()))
+        
+    @staticmethod
+    def save_json(json_data, file_name):
+        with open(file_name, 'w') as f:
+            json.dump(json_data, f, indent=4, sort_keys=True, ensure_ascii=False)
+
+
+class Data:
     
     @staticmethod
-    def json_submission(soup: bs):
-        items = soup.select('td')
-        data = {
-            'no'         : int(items[0].get_text()),
-            'user_id'    : items[1].get_text(),
-            'problem_id' : int(items[2].get_text()) if items[2].get_text() else None,
-            'result'     : items[3].get_text(),
-            'memory'     : int(items[4].get_text()) if items[4].get_text() else None,
-            'time'       : int(items[5].get_text()) if items[5].get_text() else None,
-            'language'   : items[6].get_text(),
-            'byte'       : int(items[7].get_text()) if items[7].get_text() else None,
-            'submit_time': int(items[8].select_one('a').attrs['data-timestamp'])
-        }
-        return data
-
-
-class Config:
-
-    session = requests.Session()
-    base_url = 'https://www.acmicpc.net'
-    headers = { 
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36',
-        'Referer': 'https://www.acmicpc.net/'
-    }
+    def update_members():
+        user_ids = boj.Group.members()
+        for user_id in user_ids:
+            DB.insert_member(user_id)
     
     @staticmethod
-    def reset():
-        Config.session = requests.Session()
+    def load_members():
+        return [user for user in DB.select_members()]
     
     @staticmethod
-    def request(**kwargs):
-        if 'headers' in kwargs:
-            Config.headers.update(kwargs['headers'])
-            del kwargs['headers']
-        if 'path' in kwargs:
-            kwargs['url'] = Config.base_url + kwargs['path']
-            del kwargs['path']
-        response: requests.Response = Config.session.request(**kwargs, headers=Config.headers)
-        if response.status_code != 200:
-            raise Exception(f'HTTP error: {response.status_code} {response.reason} / {response.text}')
-        return bs(response.text, 'lxml')
-    
-
-class BOJ:
-
+    def update_submissions(verbose=False):
+        members = Data.load_members()
+        for member in members[3:]:
+            submissions = boj.Status.accepted_all(member[0])
+            if verbose: print(f'{member[0]}: {len(submissions)}')
+            for submission in submissions:
+                DB.insert_submission(*submission.values())
+                
     @staticmethod
-    def accepted(username, top=None):
-        path = '/status'
-        params = { 'user_id': username, 'result_id': 4, 'top': top }
-        soup = Config.request(method='GET', path=path, params=params)
-        submissions = [ Utils.json_submission(submission) for submission in soup.select('#status-table > tbody > tr') ]
-        return submissions
-
-
-class DB:
+    def update_practices():
+        practices = boj.Group.practices()
+        for practice in practices:
+            DB.insert_practice(*practice.values())
+            problems = boj.Group.practice_problems(practice['practice_id'])
+            for problem in problems:
+                DB.insert_problem(problem['problem_id'], problem['title'])
+                DB.insert_practice_problem(practice['practice_id'], *problem.values())
     
     @staticmethod
-    def connect():
-        return sqlite3.connect('aloha.db')
+    def load_practices():
+        return [practice for practice in DB.select_practices()]
+    
+    @staticmethod
+    def load_practice_problems(practice_id):
+        return [problem for problem in DB.select_practice_problems(practice_id)]
+
+    @staticmethod
+    def calc_ac(user_id, practice_id):
+        practice = DB.get_practice(practice_id)
+        week = 7 * 24 * 60 * 60 * 1000
+        problems = [problem[2] for problem in DB.select_practice_problems(practice_id)]
+        submissions = [submission[2] for submission in DB.select_submissions(user_id, practice[2]-week, practice[3]+week)]
+        return [ac for ac in submissions if ac in problems]
 
 def main():
-    Utils.print_json(BOJ.accepted('ymjoo12', 41444978))
+    members = Data.load_members()
+    practices = Data.load_practices()
+    for practice in practices:
+        print('='*10, practice[1], '='*10)
+        for member in members:
+            print(f'{member}, {practice[0]}: {len(Data.calc_ac(member, practice[0]))}')
+        
 
 if __name__ == '__main__':
     main()
